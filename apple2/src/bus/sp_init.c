4a873dfb7f7f9b64ebb0ca8589c7c4ac71bf1c40
ShunKita <sashunic@icloud.com>
2024-06-02 05:54:18 +0900

    support multiple smart port card

--- apple2/src/bus/sp_init.c
+++ apple2/src/bus/sp_init.c
@@ -7,12 +7,15 @@ uint8_t read_memory(uint16_t address) {
 	return *((volatile uint8_t *) address);
 }
 
+
 uint8_t sp_init() {
 	const uint8_t sp_markers[] = {0x20, 0x00, 0x03, 0x00};
 	uint16_t base;
 	uint8_t i;
 	bool match;
 	uint8_t offset;
+	uint8_t fn_device;
+	uint16_t tmp_addr;
 
 	sp_network = 0;
 
@@ -28,13 +31,20 @@ uint8_t sp_init() {
 		if (match) {
 			// If a match is found, calculate the dispatch function address
 			offset = read_memory(base + 0xFF);
-			base += offset + 3;
-			sp_dispatch_fn[0] = base & 0xFF;
-			sp_dispatch_fn[1] = (base >> 8) & 0xFF;
+			tmp_addr = base + offset + 3;
+			sp_dispatch_fn[0] = tmp_addr & 0xFF;
+			sp_dispatch_fn[1] = (tmp_addr >> 8) & 0xFF;
 
-			// now find and return the network id, this will return 0 if not found.
+			// now find and return the network id.
 			sp_is_init = 1;
-			return sp_get_network_id();
+
+			fn_device = sp_get_network_id();
+			if (fn_device != 0) {
+				return fn_device;
+			}
+			else {
+				sp_is_init = 0;
+			}
 		}
 	}
 
