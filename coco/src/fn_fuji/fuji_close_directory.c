f4a700877f74ed7bccf7491ae7b1c5610bb65623
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 12:54:35 -0500

    [coco][fuji] fuji_close_directory()

--- coco/src/fn_fuji/fuji_close_directory.c
+++ coco/src/fn_fuji/fuji_close_directory.c
@@ -1,8 +1,23 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_close_directory()
 {
-	return true;
+    struct _cd
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } cd;
+
+    cd.opcode = OP_FUJI;
+    cd.cmd = FUJICMD_CLOSE_DIRECTORY;
+    
+    bus_ready();
+
+    dwwrite((uint8_t *)&cd, sizeof(cd));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
