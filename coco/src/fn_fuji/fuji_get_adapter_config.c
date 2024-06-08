ebc6a5665e928a7a0d78fffe15e76ffc841e225c
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 13:26:48 -0500

    [coco][fuji] fuji_get_adapter_config() and extended().

--- coco/src/fn_fuji/fuji_get_adapter_config.c
+++ coco/src/fn_fuji/fuji_get_adapter_config.c
@@ -1,8 +1,21 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_get_adapter_config(AdapterConfig *ac)
 {
-	return true;
+    struct _gac
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } gac;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&gac, sizeof(gac));
+    dwread((uint8_t *)ac, sizeof(AdapterConfig));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
--- coco/src/fn_fuji/fuji_get_adapter_config_extended.c
+++ coco/src/fn_fuji/fuji_get_adapter_config_extended.c
@@ -1,8 +1,21 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_get_adapter_config_extended(AdapterConfigExtended *ac)
 {
-	return true;
+    struct _gace
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } gace;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&gace, sizeof(gace));
+    dwread((uint8_t *)ac, sizeof(AdapterConfigExtended));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
