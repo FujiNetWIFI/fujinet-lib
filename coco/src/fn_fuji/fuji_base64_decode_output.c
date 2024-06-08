0971d210db7a82bc67655244a1dc94bbbc11570e
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 11:51:41 -0500

    [coco][fuji] fuji64_base64_decode_output()

--- coco/src/fn_fuji/fuji_base64_decode_output.c
+++ coco/src/fn_fuji/fuji_base64_decode_output.c
@@ -1,8 +1,24 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_base64_decode_output(char *s, uint16_t len)
 {
-	return true;
+    struct _bdo
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } bdo;
+
+    bdo.opcode = OP_FUJI;
+    bdo.cmd = FUJICMD_BASE64_DECODE_OUTPUT;
+    
+    bus_ready();
+
+    dwwrite((uint8_t *)&bdo, sizeof(bdo));
+    dwread((uint8_t *)s, len);
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
