19f469e3524b2068c481495aa80d71f862610995
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 13:52:16 -0500

    [coco][fuji] fuji_get_device_slots()

--- coco/src/fn_fuji/fuji_get_device_slots.c
+++ coco/src/fn_fuji/fuji_get_device_slots.c
@@ -1,8 +1,21 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_get_device_slots(DeviceSlot *d, size_t size)
 {
-	return true;
+    struct _gds
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } gds;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&gds, sizeof(gds));
+    dwread((uint8_t *)d, size);
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
