9b79c501e0b8131948b1f40a8748a7608dad2463
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 14:49:58 -0500

    [coco][fuji] fuji_get_wifi_*()

--- coco/src/fn_fuji/fuji_get_wifi_enabled.c
+++ coco/src/fn_fuji/fuji_get_wifi_enabled.c
@@ -1,8 +1,26 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_get_wifi_enabled()
 {
-	return true;
+    bool enabled = false;
+    
+    struct _gwe
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } gwe;
+
+    gwe.opcode = OP_FUJI;
+    gwe.cmd = FUJICMD_GET_WIFI_ENABLED;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&gwe, sizeof(gwe));
+    dwread((uint8_t *)&enabled, sizeof(bool));
+    
+    return enabled;
 }
--- coco/src/fn_fuji/fuji_get_wifi_status.c
+++ coco/src/fn_fuji/fuji_get_wifi_status.c
@@ -1,8 +1,24 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_get_wifi_status(uint8_t *status)
 {
-	return true;
+    struct _gws
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } gws;
+
+    gws.opcode = OP_FUJI;
+    gws.cmd = FUJICMD_GET_WIFISTATUS;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&gws, sizeof(gws));
+    dwread((uint8_t *)status, sizeof(uint8_t));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
