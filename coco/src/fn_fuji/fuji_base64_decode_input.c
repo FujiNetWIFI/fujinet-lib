8e10d2d7bfdf3e60bf0fcb3f90d1a555b62a1969
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 11:38:35 -0500

    [coco][fuji] fuji_base64_decode_input()

--- coco/src/fn_fuji/fuji_base64_decode_input.c
+++ coco/src/fn_fuji/fuji_base64_decode_input.c
@@ -1,8 +1,23 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_base64_decode_input(char *s, uint16_t len)
 {
-	return true;
+    struct _bdi
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } bdi;
+
+    bdi.opcode = OP_FUJI;
+    bdi.cmd = FUJICMD_BASE64_DECODE_INPUT;
+    
+    bus_ready();
+    dwwrite((uint8_t *)&bdi, sizeof(bdi));
+    dwwrite((uint8_t *)s,len);
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
