b41eb7a7125f3c4a84d404079448419a16868c72
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 14:41:50 -0500

    [coco][fuji] get_scan_result and get_ssid.

--- coco/src/fn_fuji/fuji_get_scan_result.c
+++ coco/src/fn_fuji/fuji_get_scan_result.c
@@ -1,8 +1,26 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_get_scan_result(uint8_t n, SSIDInfo *ssid_info)
 {
-	return true;
+    struct _gsr
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+        uint8_t n;
+    } gsr;
+
+    gsr.opcode = OP_FUJI;
+    gsr.cmd = FUJICMD_GET_SCAN_RESULT;
+    gsr.n = n;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&gsr, sizeof(gsr));
+    dwread((uint8_t *)ssid_info, sizeof(SSIDInfo));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
--- coco/src/fn_fuji/fuji_get_ssid.c
+++ coco/src/fn_fuji/fuji_get_ssid.c
@@ -1,8 +1,24 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_get_ssid(NetConfig *net_config)
 {
-	return true;
+    struct _gs
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } gs;
+
+    gs.opcode = OP_FUJI;
+    gs.cmd = FUJICMD_GET_SSID;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&gs, sizeof(gs));
+    dwread((uint8_t *)net_config, sizeof(NetConfig));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
