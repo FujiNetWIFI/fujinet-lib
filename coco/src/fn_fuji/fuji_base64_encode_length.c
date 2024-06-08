c64994dd7ba94c00a17723fe3f38ce79de19f453
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 12:04:27 -0500

    [coco][fuji] fuji_base64_encode_length()

--- coco/src/fn_fuji/fuji_base64_encode_length.c
+++ coco/src/fn_fuji/fuji_base64_encode_length.c
@@ -1,8 +1,24 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_base64_encode_length(unsigned long *len)
 {
-	return true;
+    struct _bel
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } bel;
+
+    bel.opcode = OP_FUJI;
+    bel.cmd = FUJICMD_BASE64_ENCODE_LENGTH;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&bel, sizeof(bel));
+    dwread((uint8_t *)len, sizeof(unsigned long));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
