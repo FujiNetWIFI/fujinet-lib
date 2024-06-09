a6eaa811317e7e2a44b7f97c329b4ac1a372344b
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 20:37:44 -0500

    [coco][fuji] fuji_read_appkey() (broken)

--- coco/src/fn_fuji/fuji_read_appkey.c
+++ coco/src/fn_fuji/fuji_read_appkey.c
@@ -1,8 +1,26 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_read_appkey(uint8_t key_id, uint16_t *count, uint8_t *data)
 {
-	return true;
+    struct _ra
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+        uint8_t key_id;
+    } ra;
+
+    ra.opcode = OP_FUJI;
+    ra.cmd = FUJICMD_READ_APP_KEY;
+    ra.key_id = key_id;
+
+    bus_ready();
+    
+    dwwrite((uint8_t *)&ra, sizeof(ra));
+    // TODO Fix this API, it's broken. shit.
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
