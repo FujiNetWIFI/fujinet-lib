588444e9e4960394ccd8e6e5adad86a85d3731f8
Mark Fisher <mark.j.fisher@gmail.com>
2024-06-09 12:04:46 +0100

    c64: fuji_appkey_open_write.c

--- commodore/src/fn_fuji/fuji_appkey_open_write.c
+++ commodore/src/fn_fuji/fuji_appkey_open_write.c
@@ -11,6 +11,7 @@ extern enum AppKeySize ak_appkey_size;
 
 bool fuji_write_appkey(uint8_t key_id, uint16_t count, uint8_t *data)
 {
+	bool ret;
 	uint8_t err = 0;
 	uint8_t mode = 0;
 	uint8_t pl[7];
@@ -30,10 +31,10 @@ bool fuji_write_appkey(uint8_t key_id, uint16_t count, uint8_t *data)
 	pl[6] = 0;					// reserved
 
 	// send the creator / app / mode values
-	if (fuji_cbm_open(FUJI_CMD_CHANNEL, FUJI_CBM_DEV, FUJI_CMD_CHANNEL, sizeof(pl), (uint8_t *) pl) != 0) {
+	if (!open_close(7, pl)) {
+		// something went wrong, abort
 		return false;
 	}
-	cbm_close(FUJI_CMD_CHANNEL);
 
 	out_data = malloc(data_size);
 	memset(out_data, 0, data_size);
@@ -42,12 +43,8 @@ bool fuji_write_appkey(uint8_t key_id, uint16_t count, uint8_t *data)
 	out_data[0] = FUJICMD_WRITE_APPKEY;
 	memcpy(&out_data[1], data, count);
 
-	err = fuji_cbm_open(FUJI_CMD_CHANNEL, FUJI_CBM_DEV, FUJI_CMD_CHANNEL, data_size, out_data);
+	ret = open_close(data_size, out_data);
 	free(out_data);
-	if (err == 0) {
-		cbm_close(FUJI_CMD_CHANNEL);
-		return true;
-	}
-	return false;
+	return ret;
 
 }
