9efc80c2967a86ea042e069617c7391c622f398b
Mark Fisher <mark.j.fisher@gmail.com>
2024-06-09 11:59:50 +0100

    c64: fuji_get_adapter_config_extended.c

--- commodore/src/fn_fuji/fuji_get_adapter_config_extended.c
+++ commodore/src/fn_fuji/fuji_get_adapter_config_extended.c
@@ -5,5 +5,7 @@
 
 bool fuji_get_adapter_config_extended(AdapterConfigExtended *ac)
 {
-	return true;
+	uint8_t pl[1];
+	pl[0] = FUJICMD_GET_ADAPTERCONFIG_EXTENDED;
+	return open_read_close(1, pl, sizeof(AdapterConfigExtended), (uint8_t *) ac);
 }
