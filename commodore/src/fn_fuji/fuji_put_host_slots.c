c22b8ee1550156f36b3e9109b971351069e65d46
Mark Fisher <mark.j.fisher@gmail.com>
2024-06-09 12:04:47 +0100

    c64: fuji_put_host_slots.c

---
+++ commodore/src/fn_fuji/fuji_put_host_slots.c
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "fujinet-fuji.h"
+#include "fujinet-fuji-cbm.h"
+
+bool fuji_put_host_slots(HostSlot *h, size_t size)
+{
+	uint16_t payload_size = sizeof(HostSlot) * size + 1;
+	uint8_t *hs_data;
+	bool ret;
+
+	hs_data = malloc(payload_size);
+	if (hs_data == NULL) {
+		return false;
+	}
+
+	hs_data[0] = FUJICMD_WRITE_HOST_SLOTS;
+	memcpy(&hs_data[1], (uint8_t *) h, payload_size - 1);
+
+	ret = open_close(payload_size, hs_data);
+	free(hs_data);
+	return ret;
+}
