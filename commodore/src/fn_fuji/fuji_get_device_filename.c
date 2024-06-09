2884795ef43f9749a1374d66017da1904817f451
Mark Fisher <mark.j.fisher@gmail.com>
2024-06-09 12:04:47 +0100

    c64: fuji_get_device_filename.c

---
+++ commodore/src/fn_fuji/fuji_get_device_filename.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "fujinet-fuji.h"
+#include "fujinet-fuji-cbm.h"
+
+bool fuji_get_device_filename(uint8_t ds, char *buffer)
+{
+	uint8_t pl[2];
+	uint8_t *filename;
+	bool is_success;
+
+	filename = malloc(256);
+	if (filename == NULL) {
+		return false;
+	}
+	memset(filename, 0, 256);
+
+	pl[0] = FUJICMD_GET_DEVICE_FULLPATH;
+	pl[1] = ds;
+
+	is_success = open_read_close(2, pl, 256, (uint8_t *) filename);
+	if (is_success) {
+		strcpy(buffer, filename);
+	}
+	free(filename);
+	return is_success;
+}
