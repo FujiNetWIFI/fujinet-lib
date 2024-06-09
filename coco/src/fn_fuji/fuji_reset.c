210136e7ef13cd8457fa6f8adc6c9b5f8e8b55f9
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 20:52:38 -0500

    [coco][fuji] fuji_reset()

--- coco/src/fn_fuji/fuji_reset.c
+++ coco/src/fn_fuji/fuji_reset.c
@@ -1,8 +1,22 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_reset()
 {
-	return true;
+    struct _r
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } r;
+
+    r.opcode = OP_FUJI;
+    r.cmd = FUJICMD_RESET;
+    
+    bus_ready();
+    dwwrite((uint8_t *)&r, sizeof(r));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
