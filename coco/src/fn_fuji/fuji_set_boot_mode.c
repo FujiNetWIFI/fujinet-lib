e83645ae9fc7587f20daaf31cff974b756247cb1
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 21:18:14 -0500

    [coco][fuji] fuji_set_boot_mode()

--- coco/src/fn_fuji/fuji_set_boot_mode.c
+++ coco/src/fn_fuji/fuji_set_boot_mode.c
@@ -1,8 +1,20 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_set_boot_mode(uint8_t mode)
 {
-	return true;
+    struct _sbm
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+        uint8_t mode;
+    } sbm;
+
+    bus_ready();
+    dwwrite((uint8_t *)&sbm, sizeof(sbm));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
