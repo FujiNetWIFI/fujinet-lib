997b84754e06564cb1fee4434a4596f69b364b82
Mark Fisher <mark.j.fisher@gmail.com>
2024-06-09 11:59:51 +0100

    c64: fuji_set_directory_position.c

--- commodore/src/fn_fuji/fuji_set_directory_position.c
+++ commodore/src/fn_fuji/fuji_set_directory_position.c
@@ -5,5 +5,9 @@
 
 bool fuji_set_directory_position(uint16_t pos)
 {
-	return true;
+	uint8_t pl[3];
+	pl[0] = FUJICMD_SET_DIRECTORY_POSITION;
+	pl[1] = pos & 0xFF;
+	pl[2] = pos >> 8;
+	return open_close(3, pl);
 }
