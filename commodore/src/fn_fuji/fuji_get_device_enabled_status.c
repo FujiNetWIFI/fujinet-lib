7571b9cc743ca3006e953d3b8422aa98f2f994de
Mark Fisher <mark.j.fisher@gmail.com>
2024-06-09 11:59:50 +0100

    c64: fuji_get_device_enabled_status.c

--- commodore/src/fn_fuji/fuji_get_device_enabled_status.c
+++ commodore/src/fn_fuji/fuji_get_device_enabled_status.c
@@ -3,6 +3,7 @@
 #include "fujinet-fuji.h"
 #include "fujinet-fuji-cbm.h"
 
+// Similar to A2, is this really a thing needed? If so, needs fixing in FN firmware
 bool fuji_get_device_enabled_status(uint8_t d)
 {
 	return true;
