92cadf8880dea092af50e21410a6e849ef8be93d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 21:02:02 -0500

    [coco][fuji] fuji_scan_for_networks()

--- coco/src/fn_fuji/fuji_scan_for_networks.c
+++ coco/src/fn_fuji/fuji_scan_for_networks.c
@@ -1,8 +1,23 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_scan_for_networks(uint8_t *count)
 {
-	return true;
+    struct _sfn
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+    } sfn;
+
+    sfn.opcode = OP_FUJI;
+    sfn.cmd = FUJICMD_SCAN_NETWORKS;
+
+    bus_ready();
+    dwwrite((uint8_t *)&sfn, sizeof(sfn));
+    bus_get_response(OP_FUJI,count,sizeof(uint8_t));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
