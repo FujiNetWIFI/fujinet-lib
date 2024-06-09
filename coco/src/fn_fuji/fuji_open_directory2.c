1466df1e707edbd80a8b19e0f110f9b465af5fca
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-08 19:39:29 -0500

    [coco][fuji] fuji_open_directory2()

--- coco/src/fn_fuji/fuji_open_directory2.c
+++ coco/src/fn_fuji/fuji_open_directory2.c
@@ -1,8 +1,27 @@
 #include <cmoc.h>
 #include <coco.h>
 #include "fujinet-fuji.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
 
 bool fuji_open_directory2(uint8_t hs, char *path, char *filter)
 {
-	return true;
+    struct _od2
+    {
+        uint8_t opcode;
+        uint8_t cmd;
+        uint8_t hs;
+        char path[256];
+    } od2;
+
+    od2.opcode = OP_FUJI;
+    od2.cmd = FUJICMD_OPEN_DIRECTORY;
+    od2.hs = hs;
+
+    strcpy(od2.path,path);
+    strcpy(&od2.path[strlen(od2.path)+1],filter);
+
+    dwwrite((uint8_t *)&od2,sizeof(od2));
+    
+    return bus_error(OP_FUJI) == BUS_SUCCESS;
 }
