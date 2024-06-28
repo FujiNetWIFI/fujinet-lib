7df1a4b3de49f4a390591eb8e9a9a8db9f9e3091
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-28 13:10:40 -0500

    [coco][fn_network] implement network_http_set_channel_mode()

--- coco/src/fn_network/network_http_set_channel_mode.c
+++ coco/src/fn_network/network_http_set_channel_mode.c
@@ -1,8 +1,29 @@
 #include <cmoc.h>
 #include <coco.h>
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
+#include <fujinet-network-coco.h>
 #include "fujinet-network.h"
 
 uint8_t network_http_set_channel_mode(char *devicespec, uint8_t mode)
 {
-	return 0;
+    struct _hscm
+    {
+        uint8_t opcode;
+        uint8_t unit;
+        uint8_t cmd;
+        uint8_t mode;
+        uint8_t unused;
+    } hscm;
+
+    hscm.opcode = OP_NET;
+    hscm.cmd = 'M';
+    hscm.unit = network_unit(devicespec);
+    hscm.mode = mode;
+    hscm.unused = 0;
+
+    bus_ready();
+    dwwrite((uint8_t *)&hscm, sizeof(hscm));
+    
+    return bus_error(OP_NET) == BUS_SUCCESS;
 }
