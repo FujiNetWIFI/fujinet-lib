e16683f747adcd686f98cddb09b8041c49713cd8
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-28 11:44:57 -0500

    [fn_network][coco] implement network_close()

--- coco/src/fn_network/network_close.c
+++ coco/src/fn_network/network_close.c
@@ -1,8 +1,26 @@
 #include <cmoc.h>
 #include <coco.h>
-#include "fujinet-network.h"
+#include <dw.h>
+#include <fujinet-network.h>
+#include <fujinet-fuji-coco.h>
+#include <fujinet-network-coco.h>
 
 uint8_t network_close(char* devicespec)
 {
-	return 0;
+    struct _nc
+    {
+        uint8_t opcode;
+        uint8_t unit;
+        uint8_t cmd;
+    } nc;
+
+    nc.opcode = OP_NET;
+    nc.unit = network_unit(devicespec);
+    nc.cmd = NETCMD_CLOSE;
+
+    bus_ready();
+
+    dwwrite((uint8_t *)&nc, sizeof(nc));
+    
+    return bus_error(OP_NET) == BUS_SUCCESS;
 }
