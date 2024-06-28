84aa4cc937e135b28ed08de3a82b757f4b8d6dce
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-28 16:36:13 -0500

    [coco][fn_network] implement network_open()

--- coco/src/fn_network/network_open.c
+++ coco/src/fn_network/network_open.c
@@ -1,8 +1,31 @@
 #include <cmoc.h>
 #include <coco.h>
-#include "fujinet-network.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
+#include <fujinet-network.h>
+#include <fujinet-network-coco.h>
 
 uint8_t network_open(char* devicespec, uint8_t mode, uint8_t trans)
 {
-	return 0;
+    struct _o
+    {
+        uint8_t opcode;
+        uint8_t unit;
+        uint8_t cmd;
+        uint8_t mode;
+        uint8_t trans;
+        char devicespec[256];
+    } o;
+
+    o.opcode = OP_NET;
+    o.unit = network_unit(devicespec);
+    o.cmd = 'O';
+    o.mode = mode;
+    o.trans = trans;
+    strcpy(o.devicespec,devicespec);
+
+    bus_ready();
+    dwwrite((uint8_t *)&o, sizeof(o));
+    
+    return bus_error(OP_NET) == BUS_SUCCESS;
 }
