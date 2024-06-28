7b1a6bd64b19e3b8fc2045ad2b90ac6a6e7c3a27
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-28 13:21:56 -0500

    [coco][fn_network] implement network_ioctl().

--- coco/src/fn_network/network_ioctl.c
+++ coco/src/fn_network/network_ioctl.c
@@ -1,8 +1,31 @@
 #include <cmoc.h>
 #include <coco.h>
-#include "fujinet-network.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
+#include <fujinet-network.h>
+#include <fujinet-network-coco.h>
 
 uint8_t network_ioctl(uint8_t cmd, uint8_t aux1, uint8_t aux2, char* devicespec, ...)
 {
-	return 0;
+    struct _ioctl
+    {
+        uint8_t opcode;
+        uint8_t unit;
+        uint8_t cmd;
+        uint8_t aux1;
+        uint8_t aux2;
+        char devicespec[256];
+    } ioctl;
+
+    ioctl.opcode = OP_NET;
+    ioctl.unit = network_unit(devicespec);
+    ioctl.cmd = cmd;
+    ioctl.aux1 = aux1;
+    ioctl.aux2 = aux2;
+    strcpy(ioctl.devicespec,devicespec);
+
+    bus_ready();
+    dwwrite((uint8_t *)&ioctl, sizeof(ioctl));
+    
+    return bus_error(OP_NET) == BUS_SUCCESS;
 }
