0086dcdca56229688b87785e31add15ec410a1f3
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-28 14:42:26 -0500

    [coco][fn_network] network_json_query, still todo.

--- coco/src/fn_network/network_json_query.c
+++ coco/src/fn_network/network_json_query.c
@@ -1,8 +1,26 @@
 #include <cmoc.h>
 #include <coco.h>
-#include "fujinet-network.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
+#include <fujinet-network.h>
+#include <fujinet-network-coco.h>
 
 int16_t network_json_query(char *devicespec, char *query, char *s)
 {
-	return 0;
+    struct _jq
+    {
+        uint8_t opcode;
+        uint8_t unit;
+        uint8_t cmd;
+        uint8_t aux1;
+        uint8_t aux2;
+        char query[256];
+    } jq;
+
+    jq.opcode = OP_NET;
+    jq.unit = network_unit(devicespec);
+    jq.cmd = 'Q';
+    jq
+    
+    return bus_error(OP_NET) == BUS_SUCCESS;
 }
