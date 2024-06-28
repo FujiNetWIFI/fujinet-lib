7daf7ff9d8de4739ae10e7ff0545757c05b12eef
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-28 16:26:21 -0500

    [coco][fn_network] finish network_json_query()

--- coco/src/fn_network/network_json_query.c
+++ coco/src/fn_network/network_json_query.c
@@ -17,9 +17,22 @@ int16_t network_json_query(char *devicespec, char *query, char *s)
         char query[256];
     } jq;
 
+    uint16_t bw=0;
+    uint8_t c=0, err=0;
+
     jq.opcode = OP_NET;
     jq.unit = network_unit(devicespec);
     jq.cmd = 'Q';
+    jq.aux1 = jq.aux2 = 0;
+    strcpy(jq.query, query);
+
+    bus_ready();
+    dwwrite((uint8_t *)&jq, sizeof(jq));
+
+    network_status(devicespec, &bw, &c, &err);
+
+    if (bw)
+        bus_get_response(OP_NET, (uint8_t *)s, bw);
     
     return bus_error(OP_NET) == BUS_SUCCESS;
 }
