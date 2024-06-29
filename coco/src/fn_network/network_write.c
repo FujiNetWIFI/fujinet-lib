13e130c0aedb9ebc85fde8be5d2aca141c00656e
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-06-28 19:10:52 -0500

    [coco][fn_network] implement network_write()

--- coco/src/fn_network/network_write.c
+++ coco/src/fn_network/network_write.c
@@ -1,8 +1,28 @@
 #include <cmoc.h>
 #include <coco.h>
-#include "fujinet-network.h"
+#include <dw.h>
+#include <fujinet-fuji-coco.h>
+#include <fujinet-network-coco.h>
+#include <fujinet-network.h>
 
 uint8_t network_write(char* devicespec, uint8_t *buf, uint16_t len)
 {
-	return 0;
+    struct _w
+    {
+        uint8_t opcode;
+        uint8_t unit;
+        uint8_t cmd;
+        uint16_t len;
+    } w;
+
+    w.opcode = OP_NET;
+    w.unit = network_unit(devicespec);
+    w.cmd = 'W';
+    w.len = len;
+
+    bus_ready();
+    dwwrite((uint8_t *)&w,sizeof(w));
+    dwwrite((uint8_t *)buf, len);
+    
+    return bus_error(OP_NET) == BUS_SUCCESS;
 }
