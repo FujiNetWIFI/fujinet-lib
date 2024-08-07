19c23108ed1787071a77500df2bbc4c42d0de81e
Eric Le Bras <eric.lebras@gmail.com>
2024-08-07 23:35:33 +0200

    Build with ORCA/M asm modules

--- makefiles/compiler-orca.mk
+++ makefiles/compiler-orca.mk
@@ -2,6 +2,7 @@ CC        := iix compile
 AR        := iix makelib
 CFLAGS    := -I -P +F cc=-p\"apple2:src:include:orca.h\" cc=-d__APPLE2__
 OBJEXT    := .root
+ASMEXT    := .asm
 
 INCC_ARG  := cc=-i
 INCS_ARG  := 
