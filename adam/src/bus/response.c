e3acef8c2b5ece97e7519f7c7233891cf7593369
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2024-08-12 13:05:01 -0500

    [adam][bus] add response buffer.

---
+++ adam/src/bus/response.c
@@ -0,0 +1,11 @@
+/**
+ * @brief   AdamNet Response Buffer
+ * @author  Thomas Cherryhomes
+ * @email   thom dot cherryhomes at gmail dot com
+ * @license gpl v. 3, see LICENSE for details.
+ * @verbose statically defined 1024 byte response used by all funcs
+ */
+
+#include "response.h"
+
+unsigned char response[RESPONSE_SIZE];
