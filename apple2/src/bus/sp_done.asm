dc434e7dfba7fd7b44cc9ffb9efb263ff9a4cb7a
Eric Le Bras <eric.lebras@gmail.com>
2024-08-30 23:52:11 +0200

    [apple2gs] New sp_done.asm

---
+++ apple2/src/bus/sp_done.asm
@@ -0,0 +1,13 @@
+                case    on              required for C compatibility
+                mcopy   13:ainclude:m16.cc
+                mcopy   13:orcainclude:m16.orca
+                mcopy   13:ainclude:M16.MiscTool
+                mcopy   13:ainclude:M16.Memory
+
+; C version 246 bytes, this is 104 or 110 bytes (65c02 vs 6502) as hand written asm
+
+sp_done         start
+
+                rtl
+
+                end
