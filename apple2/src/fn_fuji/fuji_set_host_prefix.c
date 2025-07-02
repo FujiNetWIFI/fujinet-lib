b4363fec9fca3bfc3396fbbf1afd0c8c028b4e0e
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-07-01 19:30:39 -0500

    [apple2][fn_fuji] implement fuji_set_host_prefix.c

--- apple2/src/fn_fuji/fuji_set_host_prefix.c
+++ apple2/src/fn_fuji/fuji_set_host_prefix.c
@@ -1,8 +1,29 @@
 #include <stdint.h>
+#include <string.h>
 #include "fujinet-fuji.h"
+#include "fujinet-bus-apple2.h"
 
 bool fuji_set_host_prefix(uint8_t hs, char *prefix)
 {
-	// Not implemented in A2
-	return false;
+  // Not implemented in A2
+  size_t filename_len = strlen(prefix);
+  if (filename_len >= 254)
+    {
+      return false;
+    }
+
+  sp_error = sp_get_fuji_id();
+  if (sp_error <= 0)
+    {
+      return false;
+    }
+
+  sp_payload[0] = (filename_len + 1) & 0xFF;
+  sp_payload[1] = 0;
+  sp_payload[2] = hs;
+  strcpy((char *) &sp_payload[3], prefix);
+
+  sp_error = sp_control(sp_fuji_id, FUJICMD_SET_HOST_PREFIX);
+  
+  return sp_error == 0;
 }
