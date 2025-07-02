6ef5b5aa8ab544ab4f677aa3bdafa9091fd72909
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-07-01 19:13:29 -0500

    [adam][fn_fuji] implement fuji_get_host_prefix and fuji_set_host_prefix.

--- adam/src/fn_fuji/fuji_get_host_prefix.c
+++ adam/src/fn_fuji/fuji_get_host_prefix.c
@@ -1,8 +1,30 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "fujinet-fuji.h"
+#include <eos.h>
+#include <string.h>
+
+extern unsigned char response[1024];
 
 bool fuji_get_host_prefix(uint8_t hs, char *prefix)
 {
-	return true;
+  struct _ghp
+  {
+    unsigned char cmd;
+    unsigned char hs;
+  } ghp;
+
+  memset(response,0,sizeof(response));
+  ghp.cmd = 0xE0;
+  ghp.hs = hs;
+
+  if (eos_write_character_device(0x0f,ghp,sizeof(ghp)) != 0x80)
+    return false;
+
+  if (eos_read_character_device(0x0f,response,sizeof(response)) != 0x80)
+    return false;
+
+  strcpy(prefix,response);
+  
+  return true;
 }
