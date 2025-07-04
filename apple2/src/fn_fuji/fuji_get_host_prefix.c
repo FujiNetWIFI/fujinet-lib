a49f6d69b758dd45e15a94c5762f43194c2a5c37
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-07-04 13:48:07 -0500

    [fn_fuji][apple2] i really am dumb.

--- apple2/src/fn_fuji/fuji_get_host_prefix.c
+++ apple2/src/fn_fuji/fuji_get_host_prefix.c
@@ -6,7 +6,7 @@
 // A8 to AF are now being allocated as get host prefix for slots 0 to 7.
 bool fuji_get_host_prefix(uint8_t hs, char *prefix)
 {
-  uint8_t stat = ds + 0xA8;
+  uint8_t stat = hs + 0xA8;
   
   if (sp_get_fuji_id() == 0) {
     return false;
@@ -14,7 +14,7 @@ bool fuji_get_host_prefix(uint8_t hs, char *prefix)
   
   sp_error = sp_status(sp_fuji_id, stat);
   if (sp_error == 0) {
-    memcpy(buffer, &sp_payload[0], 256);
+    memcpy(prefix, &sp_payload[0], 256);
   }
   return sp_error == 0;
 }
