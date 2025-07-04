10126199aa46469621629c69eaefacf0609d7a1b
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-07-04 14:29:54 -0500

    [pmd85][fn_fuji] implement fuji_get_host_prefix and fuji_set_host_prefix

--- pmd85/src/fn_fuji/fuji_set_host_prefix.c
+++ pmd85/src/fn_fuji/fuji_set_host_prefix.c
@@ -1,7 +1,25 @@
+#include <dw.h>
+#include <fujinet-fuji-pmd85.h>
+#include <string.h>
 #include "fujinet-fuji.h"
 
 bool fuji_set_host_prefix(uint8_t hs, char *prefix)
 {
-    // Not implemented anywhere yet.
-    return true;
+  struct _shp
+  {
+    uint8_t opcode;
+    uint8_t cmd;
+    uint8_t hs;
+    char filename[256];    
+  } shp;
+
+  shp.opcode = OP_FUJI;
+  shp.cmd = FUJICMD_SET_HOST_PREFIX;
+  shp.hs = hs;
+  strcpy(shp.filename,prefix);
+
+  bus_ready();
+  dwwrite((uint8_t *)&shp, sizeof(shp));
+
+  return !fuji_get_error();
 }
