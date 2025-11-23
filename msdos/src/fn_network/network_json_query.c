c0f9080d67390d1ff034a964f9a3ffc9c5267df5
Rich Stephens <richswww@gmail.com>
2025-11-23 08:50:14 -0500

    Fix json query line endings coco/msdos

--- msdos/src/fn_network/network_json_query.c
+++ msdos/src/fn_network/network_json_query.c
@@ -9,6 +9,8 @@ int16_t network_json_query(const char *devicespec, const char *query, char *s)
   uint8_t ret = 0;
   uint8_t c=0, err=0;
   uint16_t bw=0;
+  char ch;
+  unsigned short offset = 0;
 
   // Perform query
   ret = int_f5_write(device,'Q',0x00,0x00,(void *)query,256);
@@ -20,6 +22,18 @@ int16_t network_json_query(const char *devicespec, const char *query, char *s)
 
   if (!bw)
     return 0;
+
+  if (network_read(devicespec, (uint8_t *)s, bw) > 0)
+  {
+      // if last char is 0x9b, 0x0A or 0x0D, then set that char to nul, else just null terminate
+      ch = s[bw - 1];
+      if (ch == 0x9B || ch == 0x0A || ch == 0x0D)
+      {
+          offset = 1;
+      }
+      s[bw - offset] = '\0';
+  }
   
-  return network_read(devicespec,(uint8_t *)s, bw);
+    // return the string length (minus any trailing EOL char)
+	return bw - offset;
 }
