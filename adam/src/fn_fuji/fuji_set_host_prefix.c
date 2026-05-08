8f0e29b2e365ff781bcdf6b68be9992a9ee66542
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-05-08 09:57:58 -0500

    [adam][fn_fuji] implement fuji_get_host_prefix and fuji_set_host_prefix.

--- adam/src/fn_fuji/fuji_set_host_prefix.c
+++ adam/src/fn_fuji/fuji_set_host_prefix.c
@@ -6,5 +6,18 @@
 
 bool fuji_set_host_prefix(uint8_t hs, char *prefix)
 {
+  struct _shp
+  {
+    unsigned char cmd;
+    unsigned char hs;
+    unsigned char prefix[256];
+  } shp;
+
+  shp.cmd = 0xE2;
+  shp.hs = hs;
+  strcpy(shp.prefix,prefix);
+  
+  eos_write_character_device(0x0f,&shp,sizeof(shp));
+
   return true;
 }
