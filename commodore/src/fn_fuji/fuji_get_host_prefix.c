662445b29f05b364e62cadeefee1182e53598564
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-05-08 10:35:42 -0500

    [commodore] remove malloc strategy.

--- commodore/src/fn_fuji/fuji_get_host_prefix.c
+++ commodore/src/fn_fuji/fuji_get_host_prefix.c
@@ -7,20 +7,15 @@
 
 bool fuji_get_host_prefix(uint8_t hs, char *prefix)
 {
-  uint8_t *filename;
+  uint8_t filename[256];
   bool is_success;
   int bytes_read;
-  
-  filename = malloc(256);
-  if (filename == NULL) {
-    return false;
-  }
+
   memset(filename, 0, 256);
-  
+
   is_success = open_read_close_data_1(FUJICMD_GET_HOST_PREFIX, &bytes_read, hs, 256, (uint8_t *) filename);
   if (is_success) {
     strcpy(prefix, (char *)filename);
   }
-  free(filename);
   return is_success;
 }
--- commodore/src/fn_fuji/fuji_set_host_prefix.c
+++ commodore/src/fn_fuji/fuji_set_host_prefix.c
@@ -1,29 +1,21 @@
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <string.h>
 #include "fujinet-fuji.h"
 #include "fujinet-fuji-cbm.h"
 
+#define PL_BUF_SIZE 256
+
 bool fuji_set_host_prefix(uint8_t hs, char *prefix)
 {
-  uint8_t *pl;
+  uint8_t pl[PL_BUF_SIZE];
   uint16_t pl_len;
-  bool ret;
-  
-  pl_len = strlen(prefix) + 3 + 1; // add 1 for the null string terminator, although technically not required as we go by lengths
-  
-  pl = malloc(pl_len);
-  if (pl == NULL) {
-    status_error(ERROR_MALLOC_FAILED, FUJICMD_SET_HOST_PREFIX);
-    return false;
-  }
-  
+
+  pl_len = strlen(prefix) + 3 + 1;
+
   pl[0] = hs;
   strcpy((char *) &pl[1], prefix);
   pl[pl_len - 1] = '\0';
-  
-  ret = open_close_data(FUJICMD_SET_HOST_PREFIX, true, pl_len, pl);
-  free(pl);
-  return ret;
+
+  return open_close_data(FUJICMD_SET_HOST_PREFIX, true, pl_len, pl);
 }
