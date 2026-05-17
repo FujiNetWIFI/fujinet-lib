ddbdd4bc509441d86e559cf51edc5e4578e96576
Geoff Oltmans <oltmansg@gmail.com>
2026-05-16 22:21:11 -0500

    Fix build error for ADAM target.

--- adam/src/fn_fuji/fuji_get_host_prefix.c
+++ adam/src/fn_fuji/fuji_get_host_prefix.c
@@ -4,10 +4,6 @@
 #include "fujinet-fuji.h"
 #include "fujinet-network.h"
 #include "fujinet-network-adam.h"
-
-bool fuji_get_host_prefix(uint8_t hs, char *prefix)
-{
-#include <eos.h>
 #include <string.h>
 
 extern unsigned char response[1024];
